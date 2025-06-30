e7cb199f108608a603e483fe83db04cc5fe92ef3
R <aghilesh26@gmail.com>
2025-06-30 21:49:10 +0530

    Resolve README.md merge conflict

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
-const dbName = 'notesApp';
 const url = process.env.MONGODB_CONNECT_URI;
-const PORT = process.env.PORT || 3000;
+const dbName = 'notesApp';
+const PORT = process.env.PORT || 3001;
 
 const client = new MongoClient(url);
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -21,18 +20,18 @@ client.connect();
 app.get('/api/notes', async (req, res) => {
   const db = client.db(dbName);
   const collection = db.collection('notes');
-  const findResult = await collection.find({}).toArray();
-  res.json(findResult);
+  const notes = await collection.find({}).toArray();
+  res.json(notes);
 });
 
 app.post('/api/notes', async (req, res) => {
-  const notes = { ...req.body, createdAt: new Date().toISOString() };
   const db = client.db(dbName);
   const collection = db.collection('notes');
-  const findResult = await collection.insertOne(notes);
-  res.send({ success: true, result: findResult });
+  const note = { ...req.body, createdAt: new Date().toISOString() };
+  const result = await collection.insertOne(note);
+  res.send({ success: true, result });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
